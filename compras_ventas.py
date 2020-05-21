6363cecc0ebbb264f1e2835a693dce236ad3462e
root <root@ip-172-31-12-57.sa-east-1.compute.internal>
2020-05-21 15:17:12 -0300

    todo

--- compras_ventas.py
+++ compras_ventas.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pickle
+from colorama import Fore
 
 i = 0
 compras =[]
@@ -12,17 +13,17 @@ ventas = []
 with open("ventas.dat", "rb") as f:
     ventas = pickle.load(f)
 print("Cantidad ventas: " + str(len(ventas)))
-print(ventas)
+#print(ventas)
 
 for c in compras:
     for v in ventas:
-        if c[0] == v[0] and c[1] == v[1]:
+        if c[0] == v[0]:
             comprado = float(c[2]*c[1])
             vendido = float(v[2]*v[1])
-            print("\tCotiz: {0:.2f}".format(c[1]))
-            print("\tCerrada: "+c[0]+" GAN: {0:.2f}".format(float(vendido-comprado))+" date venta: "+str(v[4]))
+            print("\tCotiz compra: $ {0:.2f}".format(c[1]))
+            print(Fore.GREEN+"\tCerrada: "+c[0]+" GAN: $ {0:.2f}".format(float(vendido-comprado))+", Fecha compra: "+str(c[5])+", Fecha Venta: "+str(v[4])+Fore.RESET)
             break
     i +=1
-    print(str(i)+"  "+c[0]+" Compra sin vender {0:.2f}".format(c[1])+" Time: "+ c[5])
+    #print(str(i)+"  "+c[0]+" Compra sin vender {0:.2f}".format(c[1])+" Time: "+ c[5])
 
 
---
+++ operacionesViejas/20052020/compras_ventas.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import pickle
+from colorama import Fore
+
+i = 0
+compras =[]
+with open("compras.dat", "rb") as f:
+    compras = pickle.load(f)
+print("Cantidad compras: " + str(len(compras)))
+print(compras)
+
+ventas = []
+with open("ventas.dat", "rb") as f:
+    ventas = pickle.load(f)
+print("Cantidad ventas: " + str(len(ventas)))
+#print(ventas)
+
+for c in compras:
+    for v in ventas:
+        if c[0] == v[0]:
+            comprado = float(c[2]*c[1])
+            vendido = float(v[2]*v[1])
+            print("\tCotiz compra: $ {0:.2f}".format(c[1]))
+            print(Fore.GREEN+"\tCerrada: "+c[0]+" GAN: $ {0:.2f}".format(float(vendido-comprado))+" date venta: "+str(v[4])+Fore.RESET)
+            break
+    i +=1
+    #print(str(i)+"  "+c[0]+" Compra sin vender {0:.2f}".format(c[1])+" Time: "+ c[5])
+
+
