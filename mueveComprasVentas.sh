a0bbfb8ce824aed652fb3fdc5bb7658f975d79e9
root <root@ip-172-31-12-57.sa-east-1.compute.internal>
2020-05-22 09:12:38 -0300

    Script para mover a la carpeta de operacionesViejas, los archivos de compras y ventas.

--- mueveComprasVentas.sh
+++ mueveComprasVentas.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+HOY=$(date +%Y-%m-%d)
+cp /usr/src/larva/compras.dat /usr/src/larva/operacionesViejas/compras_$HOY.dat
+
+cp /usr/src/larva/ventas.dat /usr/src/larva/operacionesViejas/ventas_$HOY.dat
+
+
+python /usr/src/larva/borrarCompras.py
+python /usr/src/larva/borrarVentas.py
+
+
 
